66311810951996e7277eead4bafb63c8ee12acd6
Thomas ROGER <thomas.roger@adfab.fr>
2013-10-24 11:21:35 +0200

    add twitter account for company

--- sql/demo.sql
+++ sql/demo.sql
@@ -106,6 +106,7 @@ CREATE TABLE IF NOT EXISTS `company` (
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `address` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `phoneNumber` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `twitterAccount` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `main_image` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
@@ -114,8 +115,8 @@ CREATE TABLE IF NOT EXISTS `company` (
 -- Contenu de la table `company`
 --
 
-INSERT INTO `company` (`id`, `title`, `address`, `phoneNumber`, `main_image`) VALUES
-(1, 'Aurélie Bénard', '10 rue Marcellin Berthelot 93700 Drancy', '06 86 92 88 98', 'media/design/1-320px-Logo_de_Drancy.jpg');
+INSERT INTO `company` (`id`, `title`, `address`, `phoneNumber`, `main_image`, `twitterAccount`) VALUES
+(1, 'Aurélie Bénard', '10 rue Marcellin Berthelot 93700 Drancy', '06 86 92 88 98', 'media/design/1-320px-Logo_de_Drancy.jpg', '');
 
 -- --------------------------------------------------------
 
