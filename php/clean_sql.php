181cdeeb5f12711d6419e20d3222e562399bbbc4
Thomas ROGER <thomas.roger@adfab.fr>
2013-10-24 11:03:04 +0200

    update replace for http:// adnd https://

--- php/clean_sql.php
+++ php/clean_sql.php
@@ -14,6 +14,7 @@ $sql = file_get_contents($fileSql);
 
 # update sql for trigger 
 $sql = str_replace(array("DELIMITER //","//","DELIMITER ;"), array("",";",""), $sql);
+$sql = str_replace(array("http:;"), array("http://"), $sql);
 
 file_put_contents($fileSql.".tmp", $sql);
 ?>
